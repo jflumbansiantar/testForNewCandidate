65880a51349bf4df87f40dda65a710afcbb41714
Julia Lumbansiantar <jflumbansiantar@gmail.com>
2021-03-27 22:07:43 +0700

    machine learning with trained model

--- single_image_object_counting.py
+++ single_image_object_counting.py
@@ -11,13 +11,13 @@ import tensorflow as tf
 from utils import backbone
 from api import object_counting_api
 
-input_video = "./input_images_and_videos/sample_input_image.jpg"
+input_image = "./input_images_and_videos/photo_2020-11-15_12-43-37.jpg"
 
 # By default I use an "SSD with Mobilenet" model here. See the detection model zoo (https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
-detection_graph, category_index = backbone.set_model('ssd_mobilenet_v1_coco_2018_01_28', 'mscoco_label_map.pbtxt')
+detection_graph, category_index = backbone.set_model('ssd_mobilenet_v1_coco_2018_01_28', 'oid_bbox_trainable_label_map.pbtxt')
 
 is_color_recognition_enabled = 0
 
-result = object_counting_api.single_image_object_counting(input_video, detection_graph, category_index, is_color_recognition_enabled) # targeted objects counting
+result = object_counting_api.single_image_object_counting(input_image, detection_graph, category_index, is_color_recognition_enabled) # targeted objects counting
 
 print (result)
--- utils/backbone.py
+++ utils/backbone.py
@@ -12,15 +12,63 @@ def set_model(model_name, label_name):
 	# What model to download.
 	model_name = model_name
 	model_file = model_name + '.tar.gz'
-	download_base = 'http://download.tensorflow.org/models/object_detection/'
+	download_base = 'https://github.com/datitran/object_detector_app/raw/master/object_detection/ssd_mobilenet_v1_coco_11_06_2017'
 
 	# Path to frozen detection graph. This is the actual model that is used for the object detection.
-	path_to_ckpt = model_name + '/frozen_inference_graph.pb'
+	path_to_ckpt = 'frozen_inference_graph.pb'
 
 	# List of the strings that is used to add correct label for each box.
 	path_to_labels = os.path.join('data', label_name)
 
 	num_classes = 90
+	print(path_to_labels, '-debug')
+
+	# Download Model if it has not been downloaded yet
+	if (model_found == 0):		
+		opener = urllib.request.URLopener()
+		opener.retrieve(download_base + model_file, model_file)
+		tar_file = tarfile.open(model_file)
+		for file in tar_file.getmembers():
+		  	file_name = os.path.basename(file.name)
+		  	if 'frozen_inference_graph.pb' in file_name:
+				  tar_file.extract(file, os.getcwd())
+
+	# Load a (frozen) Tensorflow model into memory.
+	detection_graph = tf.Graph()
+	with detection_graph.as_default():
+		od_graph_def = tf.compat.v1.GraphDef()
+		with tf.compat.v2.io.gfile.GFile(path_to_ckpt, 'rb') as fid:
+			print(fid, '-fid')
+			serialized_graph = fid.read()
+			od_graph_def.ParseFromString(serialized_graph)
+			tf.import_graph_def(od_graph_def, name='')
+
+# Loading label map
+# Label maps map indices to category names, so that when our convolution network predicts 5, we know that this corresponds to airplane. Here I 		use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+	label_map = label_map_util.load_labelmap(path_to_labels)
+	categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=num_classes, use_display_name=True)
+	category_index = label_map_util.create_category_index(categories)
+
+	return detection_graph, category_index
+
+
+	for file in glob.glob("*"):
+		if (file == model_name):
+			model_found = 44
+
+	# What model to download.
+	model_name = model_name
+	model_file = model_name + '.tar.gz'
+	download_base = 'https://github.com/datitran/object_detector_app/raw/master/object_detection/ssd_mobilenet_v1_coco_11_06_2017'
+
+	# Path to frozen detection graph. This is the actual model that is used for the object detection.
+	path_to_ckpt = 'frozen_inference_graph.pb'
+
+	# List of the strings that is used to add correct label for each box.
+	path_to_labels = os.path.join('data', label_name)
+
+	num_classes = 90
+	print(path_to_labels, '-debug')
 
 	# Download Model if it has not been downloaded yet
 	if (model_found == 0):		
@@ -35,11 +83,12 @@ def set_model(model_name, label_name):
 	# Load a (frozen) Tensorflow model into memory.
 	detection_graph = tf.Graph()
 	with detection_graph.as_default():
-	  od_graph_def = tf.GraphDef()
-	  with tf.gfile.GFile(path_to_ckpt, 'rb') as fid:
-	    serialized_graph = fid.read()
-	    od_graph_def.ParseFromString(serialized_graph)
-	    tf.import_graph_def(od_graph_def, name='')
+		od_graph_def = tf.compat.v1.GraphDef()
+		with tf.compat.v2.io.gfile.GFile(path_to_ckpt, 'rb') as fid:
+			print(fid, '-fid')
+			serialized_graph = fid.read()
+			od_graph_def.ParseFromString(serialized_graph)
+			tf.import_graph_def(od_graph_def, name='')
 
 # Loading label map
 # Label maps map indices to category names, so that when our convolution network predicts 5, we know that this corresponds to airplane. Here I 		use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
